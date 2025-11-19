8f4e992dc696a4f869de5aed9fa4d621d595488d
alfgrini75 <alf.grini@gmail.com>
2025-11-19 11:23:17 +0000

    Tried to fix error

--- octofit-tracker/backend/octofit_tracker/models.py
+++ octofit-tracker/backend/octofit_tracker/models.py
@@ -1,24 +1,29 @@
-from django.db import models
+from djongo import models
 
 class Team(models.Model):
+    _id = models.ObjectIdField(primary_key=True, default=None)
     name = models.CharField(max_length=100, unique=True)
 
 class User(models.Model):
+    _id = models.ObjectIdField(primary_key=True, default=None)
     name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name='members')
 
 class Activity(models.Model):
+    _id = models.ObjectIdField(primary_key=True, default=None)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     type = models.CharField(max_length=50)
     duration = models.IntegerField()  # minutes
     date = models.DateField()
 
 class Workout(models.Model):
+    _id = models.ObjectIdField(primary_key=True, default=None)
     name = models.CharField(max_length=100)
     description = models.TextField()
     suggested_for = models.ManyToManyField(User)
 
 class Leaderboard(models.Model):
+    _id = models.ObjectIdField(primary_key=True, default=None)
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     points = models.IntegerField(default=0)
